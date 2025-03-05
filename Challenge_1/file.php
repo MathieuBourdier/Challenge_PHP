707236aac705c9269702a97379a4fb40bff8ff0e
Mathieu Bourdier <math.9408@gmail.com>
2025-03-05 23:46:48 +0100

    adding code php Challenge 1

---
+++ Challenge_1/file.php
@@ -0,0 +1,16 @@
+<?php
+$bookTitle = 'voyage de Stevenson avec un âne';
+$bookLearn = 'Non';
+$bookDate = '21/06/2017';
+$bookPrice = '6.20';
+$bookAuthor = 'Robert Louis Stevenson';
+
+
+echo "Voici le livre : $bookTitle\n";
+echo "Livre lu: $bookLearn\n";
+echo "Date de publication: $bookDate\n";
+echo "Prix: $bookPrice €\n";
+echo "Je suis écrit par $bookAuthor\n";
+
+
+?>
