4306d58f71dbde184c02ac5ab68b1f9860bc5a78
Mathieu Bourdier <math.9408@gmail.com>
2025-03-08 22:34:55 +0100

    add Challenge 6 php

---
+++ Challenge_6/file.php
@@ -0,0 +1,11 @@
+<?php
+function writeSecretSentence(string $parametre_1 , string $parametre_2):string
+{
+   return "$parametre_1 s'inclinent face au $parametre_2";
+  
+
+}
+
+echo writeSecretSentence("Donkey's","Fermier");
+
+?>
